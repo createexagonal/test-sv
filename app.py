787ad04718daaf786117c01ce4eb00644b1601fd
createexagonal <reysnico6@gmail.com>
2026-04-05 16:34:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.secret_key = os.environ.get("SECRET_KEY", "cambia-esto-en-produccion")
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
 METADATA_FILE = os.path.join(BASE_DIR, "files_metadata.json")
-ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "admin123")
+ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "francy")
 MAX_CONTENT_LENGTH = 100 * 1024 * 1024  # 100 MB
 
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
